45c00362f463e976a41455cb2f52512482482560
Beck Yuldashev <yuldashev.bek@gmail.com>
2022-01-09 17:41:01 +0300

    Added home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,19 @@ const linkAction = () => {
 navLinks.forEach(item => {
   item.addEventListener('click', linkAction);
 });
+
+
+/* =========== CHANGE BACKGROUND HEADER ============ */
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+
+  // if (this.scrollY >= 80) {
+  //   header.classList.add('scroll-header');
+  // } else {
+  //   header.classList.remove('scroll-header');
+  // }
+
+  this.scrollY >= 80 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
